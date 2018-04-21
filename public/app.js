b9c1a4c3ccb723c01f79606b71004436f26d13f7
Ross Hill <rosshill88@gmail.com>
2018-04-21 20:12:23 +0100

    Planet data generating in table, basic table styling applied

--- public/app.js
+++ public/app.js
@@ -19,14 +19,37 @@ const requestComplete = function() {
 }
 
 const populateList = function(planets) {
-    const ul = document.getElementById('planet_names');
+    // const ul = document.getElementById('planet_names');
+    const table = document.getElementById('table_body');
+    // planets.results.forEach(function(planet){
+    //     const li = document.createElement("li");
+    //     console.log(planet.name);
 
-    planets.results.forEach(function(planet){
-        const li = document.createElement("li");
-        console.log(planet.name);
+    //     li.innerText = planet.name + " " + planet.population + " " + planet.diameter;
+    //     ul.appendChild(li);
+    planets.results.forEach(function(planet) {
+        let count = 0;
+        const planet_data = document.createElement('tr');
+        const row = table.insertRow(0)
 
-        li.innerText = planet.name + " " + planet.population + " " + planet.diameter;
-        ul.appendChild(li);
+        const name = row.insertCell(0);
+        const population = row.insertCell(1);
+        const diameter = row.insertCell(2);
+        const rot_period = row.insertCell(3);
+        const orb_period = row.insertCell(4);
+        const terrain = row.insertCell(5);
+        const films = row.insertCell(6);
+
+        name.innerText = planet.name;
+        population.innerText = planet.population;
+        diameter.innerText = planet.diameter;
+        rot_period.innerText = planet.rotation_period;
+        orb_period.innerText = planet.orbital_period;
+        terrain.innerText = planet.terrain;
+        films.innerText = planet.films[0];
+
+        count++;
+
     });
 }
 
