691f1dfd7501602335ff71feaa2a54d822e0f7e5
Ross Hill <rosshill88@gmail.com>
2018-04-21 18:31:56 +0100

    Tested returning data from API

--- public/app.js
+++ public/app.js
@@ -1,34 +1,33 @@
 const app = function () {
-    // const url = "https://swapi.co/api/planets/?format=json"
-    // makeRequest(url, requestComplete);
-    console.log("loaded");
-    
+    const url = "https://swapi.co/api/planets/?format=json"
+    makeRequest(url, requestComplete);
+    console.log("loaded");   
 };
 
-// const makeRequest= function(url, callback) {
-//     const request = new XMLHttpRequest();
-//     request.open("GET", url);
-//     request.addEventListener("load", callback);
-//     request.send();
-// }
+const makeRequest= function(url, callback) {
+    const request = new XMLHttpRequest();
+    request.open("GET", url);
+    request.addEventListener("load", callback);
+    request.send();
+}
 
-// const requestComplete = function() {
-//     if(this.status !== 200) return;
-//     const jsonString = this.responseText;
-//     const planets = JSON.parse(jsonString);
-//     populateList(planets);
-// }
+const requestComplete = function() {
+    if(this.status !== 200) return;
+    const jsonString = this.responseText;
+    const planets = JSON.parse(jsonString);
+    populateList(planets);
+}
 
-// const populateList = function(planet) {
-//     const planet_div = document.getElementById('planet_div');
-//     const ul = document.getElementById('plant_names');
+const populateList = function(planets) {
+    const ul = document.getElementById('planet_names');
 
-//     planets.forEach(function(planet){
-//         const li = document.createElement("li");
+    planets.results.forEach(function(planet){
+        const li = document.createElement("li");
+        console.log(planet.name);
 
-//         li.innerText = planet.name;
-//         ul.appendChild(li);
-//     })
-// }
+        li.innerText = planet.name + " " + planet.population + " " + planet.diameter;
+        ul.appendChild(li);
+    });
+}
 
 document.addEventListener('DOMContentLoaded', app);
