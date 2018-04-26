aec3efa13d6c15359f269da994c4ec62e3e29b3b
Ross Hill <rosshill88@gmail.com>
2018-04-26 12:22:05 +0100

    Implemented realtime searching

--- public/search.js
+++ public/search.js
@@ -1,36 +1,20 @@
 import("./app.js")
 
 let searchbutton = document.getElementById('search_button');
+let search_input = document.getElementById('search_input');
 
 searchbutton.addEventListener("click", function () {
     clearTable();
     resetPageNos();
     let search_term = document.getElementById('search_input').value;
-    let url = search_url + search_term
-    console.log(url);
-    sendSearch(url);
+    let url = search_url + search_term;
+    makeRequest(url, requestComplete);
 });
 
-const sendSearch = function (url) {
-    makeRequest(url, requestComplete);
+let enter_search = function() {
+        clearTable();
+        resetPageNos();
+        let search_term = document.getElementById('search_input').value;
+        let url = search_url + search_term;
+        makeRequest(url, requestComplete);
 }
-
-// function myFunction() {
-//     clearTable();
-//     resetPageNos();
-//     let input = document.getElementById('search_input')
-//     let table_content = document.getElementById('planets_table');
-//     let table_row = table_content.getElementsByTagName("tr");
-//     let td;
-
-//     for (i = 0; i < table_row.length; i++) {
-//         td = tr[i].getElementsByTagName("td")[0];
-//         if (td) {
-//             if (td.innerHTML.indexOf(filter) > -1) {
-//                 tr[i].style.display = "";
-//             } else {
-//                 tr[i].style.display = "none";
-//             }
-//         }
-//     }
-// }
